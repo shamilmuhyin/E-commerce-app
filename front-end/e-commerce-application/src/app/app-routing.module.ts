8a6e0b60ceb1c959111d6e87f96079e150938343
SHAMIL MUHYIN K <shamilmuhyink@gmail.com>
2024-04-01 22:54:08 +0530

    "Add home component and update auth component; remove app component spec"
    This commit includes the following changes:
    
    * Adds a new HomeComponent to the app-routing.module.ts file, with an empty path and the HomeComponent as its component.
    * Updates the AuthComponent's HTML template to include a form for logging in, with form controls for the username and password and validation messages for required fields and invalid input.
    * Updates the AuthComponent's SCSS file to set the width and height of the background image.
    * Removes the AppComponent's spec file, which contained unit tests for the AppComponent.

--- front-end/e-commerce-application/src/app/app-routing.module.ts
+++ front-end/e-commerce-application/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './module/auth/auth.component';
+import { HomeComponent } from './module/home/home.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
   {
     path: 'auth',
     component: AuthComponent
---
+++ front-end/e-commerce-application/src/app/module/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'my-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent implements OnInit{
+
+  constructor() {}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+}
